f81ab3564bb11eb3c6db131af8c8ff30ee72431c
shubhas1994 <33656112+shubhas1994@users.noreply.github.com>
2017-11-14 16:46:45 +0530

    commenting
    asfdsdg

--- src/ricchagth/Testng.java
+++ src/ricchagth/Testng.java
@@ -3,7 +3,7 @@ package ricchagth;
 public class Testng {
 
 	public static void main(String[] args) {
-	System.out.println("riccha is good gal");
+	//System.out.println("riccha is good gal");
 
 	}
 
